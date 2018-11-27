5495b3f9bdd34a343dfe92f24d114b683501af0c
Taesu Hyeon <qvil1127@gmail.com>
2018-11-28 02:00:15 +0900

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
-var path = require('path');
+var path = require("path");
+var nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'lte-earfcn-calculator.min.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['babili']
-                }
-            }
-        ]
-    }
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "lte-earfcn-calculator.min.js",
+    library: "",
+    libraryTarget: "commonjs"
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
